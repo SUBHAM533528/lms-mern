40e2fedecadade13ec86d8062e453f2a6adffe43
SUBHAM533528 <subhammaharana412@gmail.com>
2026-05-10 00:33:11 +0530

    fix the server file

--- server/server.js
+++ server/server.js
@@ -1,31 +1,30 @@
 import express from 'express'
 import cors from 'cors'
-import 'dotenv/config' 
+import 'dotenv/config'
 import connectDb from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
-
 
 // initialize express
 const app = express()
 
-// Connect Mongodb
+// Connect MongoDB
 await connectDb()
 
-// Middlewares
-app.use("/api/webhooks", clerkWebhooks)
+// Clerk webhook route FIRST
+app.post('/api/webhooks', clerkWebhooks)
 
+// Middlewares
 app.use(cors())
-app.use(express.json());
+app.use(express.json())
 
 // Routes
-app.get('/', (req, res) => res.send('Api Is Working'))
-app.post('/clerk', express.json(), clerkWebhooks)
-
-// Port
-const port = process.env.port || 5000
-
-app.listen(port, ()=> {
-    console.log(`server is running successfully on port ${port}`)
+app.get('/', (req, res) => {
+    res.send('API Is Working')
 })
 
+// Port
+const port = process.env.PORT || 5000
 
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
